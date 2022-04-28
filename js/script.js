b788c94851b955afc7d631605f2f23ef8e3c1582
Baherian Serhiy <98645818+Baheryan@users.noreply.github.com>
2022-04-28 22:18:39 +0300

    Add files via upload
    uploading initial js files

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('.buttonMenu').click(function(event) {
+        $('.buttonMenu, .header__nav, .header__button__container, .color__switch').toggleClass('active');
+    });
+});
