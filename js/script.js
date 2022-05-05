710fdcac690829c03ab1a34ff9362ac818cf0372
Baherian Serhiy <98645818+Baheryan@users.noreply.github.com>
2022-05-05 22:27:36 +0300

    Update script.js
    adding color switch function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
+//buttonMenu click animation
+
 $(document).ready(function() {
     $('.buttonMenu').click(function(event) {
         $('.buttonMenu, .header__nav, .header__button__container, .color__switch').toggleClass('active');
     });
 });
+
+
+//color theme switch
+
+var switchMode = document.getElementById("switchMode");
+
+switchMode.onclick = function() {
+
+    var theme = document.getElementById("theme");
+
+    if (theme.getAttribute("href") == "styles/styles.css") {
+
+        theme.href = "styles/lightTheme.css";
+    }
+
+    else {
+
+        theme.href = "styles/styles.css";
+    }
+}
